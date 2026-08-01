9b3367c8b8c65b11e3dd9ad79ccf7542a1495e86
Comeat0205 <3329445208@qq.com>
2026-08-01 19:15:01 +0800

    lab1完成

--- user/xargs.c
+++ user/xargs.c
@@ -1,68 +1,95 @@
 #include "kernel/types.h"
-#include "user/user.h"
+#include "kernel/param.h"
 #include "kernel/stat.h"
+#include "user/user.h"
 
-#define MAXARGS 16
-#define BUFSIZE 128
+// Read one line from stdin into buf (up to n-1 bytes).
+// Returns number of bytes stored (excluding NUL), 0 on EOF with empty line, -1 on EOF.
+int
+readline(char *buf, int n)
+{
+  int i = 0;
+  char c;
 
-// 按换行分割一行字符串，填入argv数组，返回参数个数
-int split(char *buf, char **argv) {
-    int n = 0;
-    char *p = buf;
-    while (*p != '\0' && n < MAXARGS - 1) {
-        // 跳过空格
-        while (*p == ' ') p++;
-        if (*p == '\0' || *p == '\n') break;
-        argv[n++] = p;
-        // 走到下一个分隔符
-        while (*p != ' ' && *p != '\n' && *p != '\0') p++;
-        if (*p != '\0') *p++ = '\0';
+  while(i < n - 1){
+    int r = read(0, &c, 1);
+    if(r < 1){
+      if(i == 0)
+        return -1; // EOF, nothing read
+      break;       // EOF after some chars: treat as end of line
     }
-    argv[n] = 0;
-    return n;
+    if(c == '\n')
+      break;
+    buf[i++] = c;
+  }
+  buf[i] = '\0';
+  return i;
 }
 
-int main(int argc, char *argv[]) {
-    // 没有传入命令，直接报错退出
-    if (argc < 2) {
-        fprintf(2, "xargs: need command arguments\n");
-        exit(1);
+// Split a line into whitespace-separated arguments.
+// Mutates buf by inserting NULs. Returns argc for the appended args.
+int
+split(char *buf, char **argv, int maxargs)
+{
+  int n = 0;
+  char *p = buf;
+
+  while(*p != '\0' && n < maxargs){
+    while(*p == ' ' || *p == '\t')
+      p++;
+    if(*p == '\0')
+      break;
+    argv[n++] = p;
+    while(*p != '\0' && *p != ' ' && *p != '\t')
+      p++;
+    if(*p != '\0')
+      *p++ = '\0';
+  }
+  return n;
+}
+
+int
+main(int argc, char *argv[])
+{
+  char buf[512];
+  char *xargv[MAXARG];
+  int i;
+
+  if(argc < 2){
+    fprintf(2, "usage: xargs command [args...]\n");
+    exit(1);
+  }
+  if(argc >= MAXARG){
+    fprintf(2, "xargs: too many arguments\n");
+    exit(1);
+  }
+
+  // Fixed command prefix from the command line.
+  for(i = 1; i < argc; i++)
+    xargv[i - 1] = argv[i];
+
+  while(readline(buf, sizeof(buf)) >= 0){
+    int nbase = argc - 1;
+    int nadd = split(buf, xargv + nbase, MAXARG - nbase - 1);
+    xargv[nbase + nadd] = 0;
+
+    // Empty line: still run the command with only the fixed args
+    // (matches common xargs -n 1 behavior for blank lines: skip if no args added)
+    if(nadd == 0)
+      continue;
+
+    int pid = fork();
+    if(pid < 0){
+      fprintf(2, "xargs: fork failed\n");
+      exit(1);
     }
-
-    char buf[BUFSIZE];
-    char *exec_argv[MAXARGS];
-
-    while (1) {
-        // 复制原有命令到执行参数数组
-        int arg_cnt = 0;
-        for (int i = 1; i < argc; i++) {
-            exec_argv[arg_cnt++] = argv[i];
-        }
-
-        // 读取一行输入
-        int len = read(0, buf, BUFSIZE - 1);
-        if (len <= 0) break; // EOF 结束循环
-        buf[len] = '\0';
-
-        // 分割当前行的参数，追加到执行列表
-        char *split_args[MAXARGS];
-        int n = split(buf, split_args);
-        for (int i = 0; i < n && arg_cnt < MAXARGS - 1; i++) {
-            exec_argv[arg_cnt++] = split_args[i];
-        }
-        exec_argv[arg_cnt] = 0;
-
-        // fork 执行命令
-        int pid = fork();
-        if (pid == 0) {
-            exec(exec_argv[0], exec_argv);
-            // exec 失败会走到这里
-            fprintf(2, "xargs: exec %s failed\n", exec_argv[0]);
-            exit(1);
-        } else {
-            wait(0); // 父进程等待子进程结束
-        }
+    if(pid == 0){
+      exec(xargv[0], xargv);
+      fprintf(2, "xargs: exec %s failed\n", xargv[0]);
+      exit(1);
     }
+    wait(0);
+  }
 
-    exit(0);
+  exit(0);
 }
